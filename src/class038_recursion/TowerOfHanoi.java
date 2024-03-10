417ccd4f5e4fc711ee4ac2a4bee083de877450a9
Melonhide <tt2830@columbia.edu>
2024-03-10 00:55:20 -0500

    Tower of Hanoi solution

--- src/class038_recursion/TowerOfHanoi.java
+++ src/class038_recursion/TowerOfHanoi.java
@@ -2,14 +2,20 @@ package class038_recursion;
 
 public class TowerOfHanoi {
     public static void hanoi(int n){
-
+        process(n, "left", "mid", "right");
     }
 
-    public static void process(){
-
+    public static void process(int n, String from, String other, String to){
+        if(n == 1){
+            System.out.println("Move "+n+" from "+ from + " to " + to);
+            return;
+        }
+        process(n-1, from, to ,other);
+        System.out.println("Move "+n+" from "+ from + " to " + to);
+        process(n-1, other, from , to);
     }
 
     public static void main(String[] args){
-
+        hanoi(3);
     }
 }
