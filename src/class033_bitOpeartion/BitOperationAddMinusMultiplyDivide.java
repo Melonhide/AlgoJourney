e39c2acc946155ccb1e5216123685948d08dfa87
Melonhide <tt2830@columbia.edu>
2024-03-17 12:04:24 -0400

    complete Add method through bit operation

--- src/class033_bitOpeartion/BitOperationAddMinusMultiplyDivide.java
+++ src/class033_bitOpeartion/BitOperationAddMinusMultiplyDivide.java
@@ -1,4 +1,27 @@
 package class033_bitOpeartion;
 
 public class BitOperationAddMinusMultiplyDivide {
+    public static void main(String[] args){
+        for(int i = 0; i < 10000; i++){
+            int a = (int)(Math.random()*(100000000));
+            int b = (int)(Math.random()*(100000000));
+            if((a+b)!=add(a,b)){
+                System.out.println("Error!!!!!!!!!!!!");
+                System.out.println(a);
+                System.out.println(b);
+            }
+            System.out.println(a);
+            System.out.println(b);
+
+        }
+
+
+    }
+
+    public static int add(int a, int b){
+        if((a&b)==0){
+            return a^b;
+        }
+        return add(a^b, (a&b)<<1);
+    }
 }
