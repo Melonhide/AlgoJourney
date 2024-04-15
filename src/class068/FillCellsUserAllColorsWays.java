c7eb8ed8b32795038598d9e9d0e10a3e1e2bf0f0
Melonhide <tt2830@columbia.edu>
2024-04-14 22:46:34 -0400

    Add Main function. No testing link, need to use test case generator

--- src/class068/FillCellsUserAllColorsWays.java
+++ src/class068/FillCellsUserAllColorsWays.java
@@ -1,4 +1,7 @@
 package class068;
 
 public class FillCellsUserAllColorsWays {
+    public static void main(String[] args){
+
+    }
 }
--- src/class068/MinimumDeleteBecomeSubstring.java
+++ src/class068/MinimumDeleteBecomeSubstring.java
@@ -1,4 +1,7 @@
 package class068;
 
 public class MinimumDeleteBecomeSubstring {
+    public static void main(String[] args){
+
+    }
 }
