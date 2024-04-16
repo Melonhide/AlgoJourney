8e06ccc02d42b0d2d0d552ac90789f0a984fce8e
Melonhide <tt2830@columbia.edu>
2024-04-15 23:07:22 -0400

    Class068 in progress

--- src/class068/MinimumDeleteBecomeSubstring.java
+++ src/class068/MinimumDeleteBecomeSubstring.java
@@ -1,6 +1,15 @@
 package class068;
 
 public class MinimumDeleteBecomeSubstring {
+
+    public static int minDelete1(String s1, String s2){
+
+    }
+
+    public static int minDelete2(String str1, String str2){
+
+    }
+
     public static void main(String[] args){
 
     }
