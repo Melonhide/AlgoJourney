6e3ca92b21c75f0fcb45a23b122c294e8711f4d2
Melonhide <tt2830@columbia.edu>
2024-05-02 20:38:07 -0400

    BuyingHayMinimumCost in progress

--- src/class074/BuyingHayMinimumCost.java
+++ src/class074/BuyingHayMinimumCost.java
@@ -1,4 +1,36 @@
 package class074;
 
+import java.io.*;
+
 public class BuyingHayMinimumCost {
+
+    public static int n, h;
+    public static int maxn = 101;
+    public static int maxh = 50001;
+    public static int[] cost = new int[maxn];
+    public static int[] val = new int[maxn];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken()!=StreamTokenizer.TT_EOF){
+            n = (int) in.nval;
+            in.nextToken();
+            h = (int) in.nval;
+            for(int i = 1; i <= n; i++){
+                in.nextToken();
+                val[i] = (int) in.nval;
+                in.nextToken();
+                cost[i] = (int) in.nval;
+            }
+            out.println(compute());
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    public static int compute(){
+
+    }
 }
