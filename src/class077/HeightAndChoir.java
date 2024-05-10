56a29e62c9234d704330fec67692b4ac4e22d875
Melonhide <tt2830@columbia.edu>
2024-05-09 23:12:19 -0400

    add comments to class077 questions

--- src/class077/HeightAndChoir.java
+++ src/class077/HeightAndChoir.java
@@ -1,4 +1,10 @@
 package class077;
 
+// 合唱队
+// 具体描述情打开链接查看
+// 测试链接 : https://www.luogu.com.cn/problem/P3205
+// 请同学们务必参考如下代码中关于输入、输出的处理
+// 这是输入输出处理效率很高的写法
+// 提交以下的所有代码，并把主类名改成"Main"，可以直接通过
 public class HeightAndChoir {
 }
--- src/class077/MinimumInsertionsToMatch.java
+++ src/class077/MinimumInsertionsToMatch.java
@@ -1,4 +1,10 @@
 package class077;
 
+// 合并石头的最低成本
+// 有 n 堆石头排成一排，第 i 堆中有 stones[i] 块石头
+// 每次 移动 需要将 连续的 k 堆石头合并为一堆，而这次移动的成本为这 k 堆中石头的总数
+// 返回把所有石头合并成一堆的最低成本
+// 如果无法合并成一堆返回-1
+// 测试链接 : https://leetcode.cn/problems/minimum-cost-to-merge-stones/
 public class MinimumInsertionsToMatch {
 }
