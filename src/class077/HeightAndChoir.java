5cd25d17b68f53313236ea294c1ae198515ff714
Melonhide <tt2830@columbia.edu>
2024-05-12 23:42:13 -0400

    Height and choir update

--- src/class077/HeightAndChoir.java
+++ src/class077/HeightAndChoir.java
@@ -1,5 +1,7 @@
 package class077;
 
+import java.io.*;
+
 // 合唱队
 // 具体描述情打开链接查看
 // 测试链接 : https://www.luogu.com.cn/problem/P3205
@@ -7,4 +9,61 @@ package class077;
 // 这是输入输出处理效率很高的写法
 // 提交以下的所有代码，并把主类名改成"Main"，可以直接通过
 public class HeightAndChoir {
+    public static int mod = 19650827;
+    public static int n;
+    public static int maxn = 1001;
+    public static int[] arr = new int[maxn];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken()!=StreamTokenizer.TT_EOF){
+            n = (int) in.nval;
+            for(int i = 0; i < n; i++){
+                in.nextToken();
+                arr[i] = (int) in.nval;
+            }
+            out.println(compute());
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    public static int compute(){
+        int[] ans = f(0, n-1);
+        return (ans[0]%mod+ans[1]%mod)%mod;
+    }
+    public static int[] f(int l , int r){
+        if(l == r-1){
+            if(arr[l]<arr[r]){
+                return new int[]{1,1};
+            }else{
+                return new int[]{0,0};
+            }
+        }
+
+
+        int left = 0;
+        int right = 0;
+        int[] tmp = f(l+1, r);
+        if(arr[l]<arr[l+1]){
+            left = (left + tmp[0])%mod;
+        }
+        if(arr[l]<arr[r]){
+            left = (left+tmp[1])%mod;
+        }
+
+        tmp = f(l, r-1);
+        if(arr[r]>arr[r-1]){
+            right = (right + tmp[1])%mod;
+        }
+        if(arr[r]>arr[l]){
+            right = (right+tmp[0])%mod;
+        }
+
+        return new int[]{left, right};
+
+
+    }
 }
