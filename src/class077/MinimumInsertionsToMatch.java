99749d51bf5a445e1a3fd301d83dc3bb390c798f
Melonhide <tt2830@columbia.edu>
2024-05-13 22:21:05 -0400

    Minimum Insertions to Match-Complete

--- src/class077/MinimumInsertionsToMatch.java
+++ src/class077/MinimumInsertionsToMatch.java
@@ -1,5 +1,7 @@
 package class077;
 
+import java.io.*;
+
 // 完成配对需要的最少字符数量
 // 给定一个由'['、']'、'('，')'组成的字符串
 // 请问最少插入多少个括号就能使这个字符串的所有括号正确配对
@@ -10,4 +12,43 @@ package class077;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class MinimumInsertionsToMatch {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        out.println(compute(str));
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    public static int compute(String str){
+        char[] s = str.toCharArray();
+        int n = s.length;
+
+        return f(s, 0,n-1);
+    }
+    public static int f(char[] s, int l , int r){
+        if(l==r){
+            return 1;
+        }
+
+        if(l == r-1){
+            if((s[l] == '(' && s[r] == ')') || (s[l] == '[' && s[r] == ']')){
+                return 0;
+            }else{
+                return 2;
+            }
+        }
+
+        int p1 = Integer.MAX_VALUE;
+        if((s[l] == '(' && s[r] == ')') || (s[l] == '[' && s[r] == ']')){
+            p1 = f(s, l+1, r-1);
+        }
+        int p2 = Integer.MAX_VALUE;
+        for(int m = l ; m<r; m++){
+            p2 = Math.min(f(s, l, m)+f(s, m+1, r), p2);
+        }
+        return Math.min(p1,p2);
+    }
 }
