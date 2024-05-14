ef87133bb196db554ad85d9495baf7466e41c1d2
Melonhide <tt2830@columbia.edu>
2024-05-13 22:33:58 -0400

    Minimum Insertions to Match; dp-passed

--- src/class077/MinimumInsertionsToMatch.java
+++ src/class077/MinimumInsertionsToMatch.java
@@ -25,8 +25,32 @@ public class MinimumInsertionsToMatch {
     public static int compute(String str){
         char[] s = str.toCharArray();
         int n = s.length;
+        int[][] dp = new int[n][n];
 
-        return f(s, 0,n-1);
+        for(int i = 0; i < n; i++){
+            dp[i][i] = 1;
+            if(i+1<n){
+                if(!((s[i] == '(' && s[i+1] == ')') || (s[i] == '[' && s[i+1] == ']'))){
+                    dp[i][i+1] = 2;
+                }
+            }
+        }
+
+        for(int l = n-3; l>=0; l--){
+            for(int r = l+2, p1 ,p2; r<n; r++){
+                p1 = Integer.MAX_VALUE;
+                p2 = Integer.MAX_VALUE;
+                if((s[l] == '(' && s[r] == ')') || (s[l] == '[' && s[r] == ']')){
+                    p1 = dp[l+1][r-1];
+                }
+                for(int m = l; m<r; m++){
+                    p2 = Math.min(p2, dp[l][m]+dp[m+1][r]);
+                }
+                dp[l][r] = Math.min(p1,p2);
+            }
+        }
+
+        return dp[0][n-1];
     }
     public static int f(char[] s, int l , int r){
         if(l==r){
