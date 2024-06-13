5c858fd15b71c29642509b3dfcd1d77f7fb8abbe
Melonhide <tt2830@columbia.edu>
2024-06-12 23:19:10 -0400

    Add comments for class083 question

--- src/class083/LongestSubarraySumNoMoreK.java
+++ src/class083/LongestSubarraySumNoMoreK.java
@@ -1,4 +1,11 @@
 package class083;
-
+// 累加和不大于k的最长子数组
+// 给定一个无序数组arr，长度为n，其中元素可能是正、负、0
+// 给定一个整数k，求arr所有的子数组中累加和不大于k的最长子数组长度
+// 要求时间复杂度为O(n)
+// 测试链接 : https://www.nowcoder.com/practice/3473e545d6924077a4f7cbc850408ade
+// 请同学们务必参考如下代码中关于输入、输出的处理
+// 这是输入输出处理效率很高的写法
+// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class LongestSubarraySumNoMoreK {
 }
