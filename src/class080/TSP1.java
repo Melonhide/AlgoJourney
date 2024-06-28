d863dd1310a8571da235a6ad69ef862ab56242ed
Melonhide <tt2830@columbia.edu>
2024-06-28 14:16:58 -0400

    TSP question complete; Need space optimiztaion to pass;

--- src/class080/TSP1.java
+++ src/class080/TSP1.java
@@ -15,15 +15,61 @@ import java.io.*;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class TSP1 {
+    public static int maxn = 19;
+    public static int n;
+    public static int[][] graph = new int[maxn][maxn];
+    public static int[][] dp = new int[1<<maxn][maxn];
+
+    public static void build(){
+        for(int i = 0; i < (1<<n); i++){
+            for(int j = 0; j < n; j++){
+                dp[i][j] = -1;
+            }
+        }
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StreamTokenizer in = new StreamTokenizer(br);
         PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
         while(in.nextToken()!=StreamTokenizer.TT_EOF){
+            n = (int) in.nval;
+            build();
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j<n; j++){
+                    in.nextToken();
+                    graph[i][j] = (int) in.nval;
+                }
+            }
 
+            out.println(compute());
         }
         out.flush();
         out.close();
         br.close();
+    }
+
+    public static int compute(){
+        return f(1, 0);
+    }
+
+    public static int f(int s, int i){
+        if(dp[s][i] != -1){
+            return dp[s][i];
+        }
+
+        if(s == ((1<<n)-1)){
+            dp[s][i] = graph[i][0];
+            return dp[s][i];
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int j = 1; j < n; j++){
+            if((s & (1<<j)) == 0){
+                ans = Math.min(ans, graph[i][j] + f((s|(1<<j)), j));
+            }
+        }
+
+        dp[s][i] = ans;
+        return ans;
     }
 }
