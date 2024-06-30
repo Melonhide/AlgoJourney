774f7b9ab2b268d593c8d20baefb3037afe48682
Melonhide <tt2830@columbia.edu>
2024-06-30 17:30:53 -0400

    Longest Subarray Sum No More K; Complete

--- src/class083/LongestSubarraySumNoMoreK.java
+++ src/class083/LongestSubarraySumNoMoreK.java
@@ -7,5 +7,68 @@ package class083;
 // 请同学们务必参考如下代码中关于输入、输出的处理
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
+
+
+import java.io.*;
+
 public class LongestSubarraySumNoMoreK {
+    public static int n, k;
+    public static int maxn = 100001;
+    public static int[] nums = new int[maxn];
+    public static int[] presum = new int[maxn];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken() != StreamTokenizer.TT_EOF){
+            n = (int) in.nval;
+            in.nextToken();
+            k = (int) in.nval;
+            for(int i = 0; i < n; i++){
+                in.nextToken();
+                nums[i] = (int) in.nval;
+            }
+            out.println(compute1());
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    //   [3, -2, -4, 0, 6]
+    //   [.., 6] <= -2 [..]<= -8
+    //[0, 3,  3,  3, 3, 3]
+    public static int compute1(){
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            presum[i+1] = Math.max(presum[i], sum);
+        }
+
+        int ans = 0;
+        for(int i = n-1, pos; i >= 0; sum -= nums[i],  i--){
+            pos = find(sum-k);
+            if(pos != -1){
+                ans = Math.max(ans, i-pos+1);
+            }
+        }
+
+        return ans;
+    }
+
+    public static int find(int x){
+        int l = 0;
+        int r = n;
+        int ans = -1;
+        while(l<=r){
+            int m = (l+r)/2;
+            if(presum[m] >= x){
+                ans = m;
+                r = m-1;
+            }else{
+                l = m+1;
+            }
+        }
+        return ans;
+    }
 }
---
+++ src/class083/LongestSubarrySumNoMoreK_optimized.java
@@ -0,0 +1,66 @@
+package class083;
+
+import java.io.*;
+
+public class LongestSubarrySumNoMoreK_optimized {
+    public static int maxn = 100001;
+    public static int[] nums = new int[maxn];
+    public static int[] minSums = new int[maxn];
+    public static int[] minSumEnds = new int[maxn];
+    public static int n, k;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken()!=StreamTokenizer.TT_EOF){
+            n = (int) in.nval;
+            in.nextToken();
+            k = (int) in.nval;
+            for(int i = 0; i < n; i++){
+                in.nextToken();
+                nums[i] = (int) in.nval;
+            }
+            out.println(compute());
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    public static int compute(){
+        minSums[n-1] = nums[n-1];
+        minSumEnds[n-1] = n-1;
+        for(int i = n-2; i >= 0; i--){
+            //   ... i, i+1, ... ,n-1
+            if(nums[i]<nums[i]+minSums[i+1]){
+                minSums[i] = nums[i];
+                minSumEnds[i] = i;
+            }else{
+                minSums[i] = nums[i]+minSums[i+1];
+                minSumEnds[i] = minSumEnds[i+1];
+            }
+        }
+
+        // minSums[5] = -9
+        // minSumEnds[5] = 8
+        // 0,0,0,0,0,-3,-3,0,-3,5,5 ,5
+        // 0,1,2,3,4, 5, 6,7, 8,9,10,11
+        int ans = 0;
+        for(int i = 0, sum = 0, end = 0; i<n; i++){
+            while(end<n && sum+minSums[end]<=k){
+                sum += minSums[end];
+                end = minSumEnds[end]+1;
+            }
+            if(i == end){
+                end++;
+            }else {
+                ans = Math.max(end - i, ans);
+                sum -= nums[i];
+            }
+        }
+        return ans;
+    }
+
+
+}
