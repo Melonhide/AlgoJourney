407262988d462a28bb9b8167a349979d277ffc18
Melonhide <tt2830@columbia.edu>
2024-07-02 08:04:24 -0400

    Added Meng number

--- src/class085/WindyNumber.java
+++ src/class085/WindyNumber.java
@@ -1,4 +1,12 @@
 package class085;
 
+// windy数
+// 不含前导零且相邻两个数字之差至少为2的正整数被称为windy数
+// windy想知道[a,b]范围上总共有多少个windy数
+// 测试链接 : https://www.luogu.com.cn/problem/P2657
+// 请同学们务必参考如下代码中关于输入、输出的处理
+// 这是输入输出处理效率很高的写法
+// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
+
 public class WindyNumber {
 }
