aae8de2c80a199b5153c7d35450fb4e3e7b19de8
Melonhide <tt2830@columbia.edu>
2024-07-05 21:09:27 -0400

    Meng number in process

--- src/class085/MengNumber.java
+++ src/class085/MengNumber.java
@@ -1,6 +1,6 @@
 package class085;
 
-import java.io.IOException;
+import java.io.*;
 
 // 萌数
 // 如果一个数字，存在长度至少为2的回文子串，那么这种数字被称为萌数
@@ -12,7 +12,19 @@ import java.io.IOException;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class MengNumber {
+    public static int l, r;
     public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken()!=StreamTokenizer.TT_EOF){
+            l = (int) in.nval;
+            in.nextToken();
+            r = (int) in.nval;
 
+        }
+        out.flush();
+        out.close();
+        br.close();
     }
 }
