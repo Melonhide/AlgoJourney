3c35d60454828ed96ddf0aa102910e8374e58243
Melonhide <tt2830@columbia.edu>
2024-07-10 22:28:50 -0400

    Diving in process

--- src/class086/Diving1.java
+++ src/class086/Diving1.java
@@ -1,6 +1,8 @@
 package class086;
 
 
+import java.io.*;
+
 // 潜水的最大时间与方案
 // 一共有n个工具，每个工具都有自己的重量a、阻力b、提升的停留时间c
 // 因为背包有限，所以只能背重量不超过m的工具
@@ -15,4 +17,48 @@ package class086;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class Diving1 {
+    public static int m,v,n;
+    public static int maxn = 101, maxm = 201, maxv = 201;
+    public static int[][] nums = new int[maxn][3];
+    public static int[][][] dp = new int[maxn][maxm][maxv];
+    public static String[][][] path = new String[maxn][maxm][maxv];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken()!=StreamTokenizer.TT_EOF){
+            m = (int) in.nval;
+            in.nextToken();
+            v = (int) in.nval;
+            in.nextToken();
+            n = (int) in.nval;
+            for(int i = 0; i<n; i++){
+                in.nextToken();
+                nums[i][0] = (int) in.nval;
+                in.nextToken();
+                nums[i][1] = (int) in.nval;
+                in.nextToken();
+                nums[i][2] = (int) in.nval;
+            }
+            out.println(compute());
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    public static int compute(){
+       for(int i = n-1, ans; i>=0; i--){
+           for(int j = m; j >=0; j--){
+               for(int k = v; k>=0; k--){
+                   dp[i][j][k] = dp[i+1][j][k];
+                   if(nums[i][0]+j<=m && nums[i][1]+k<=v){
+                       dp[i][j][k] = Math.max(dp[i][j][k], nums[i][2]+dp[i+1][nums[i][0]+j][nums[i][1]+k]);
+                   }
+               }
+           }
+       }
+       return dp[0][0][0];
+    }
 }
