c1976b40ba2b645c8e615eff1af515cca94101d9
Melonhide <tt2830@columbia.edu>
2024-11-09 23:30:22 -0500

    Update 087 question

--- src/class087/PickNumbersClosedSum.java
+++ src/class087/PickNumbersClosedSum.java
@@ -22,11 +22,77 @@ public class PickNumbersClosedSum {
         if(sumMin>target||sumMax<target){
             return new int[0];
         }
+
+        return generate(target, n, k);
     }
 
     public static int[] generate(long sum, int n, int k){
+        int range = n-k;
+        long cur = (1+k)*k/2;
+        long need = sum-cur;
+        int[] ans = new int[k];
+        for(int i = 1; i <= k; i++){
+            ans[i-1] = i;
+        }
+        int move =(int) need/range;
+        int left =(int) need % range;
+        int ind = 0;
+        while(move != 0){
+            ans[ind++] += range;
+            move--;
+        }
+        if(left != 0){
+            ans[ind] += left;
+        }
 
+        return ans;
     }
+
+    public static boolean pass(int n, int k, int[] ans) {
+        if (ans.length == 0) {
+            if (canSplit(n, k)) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            if (ans.length != k) {
+                return false;
+            }
+            int sum = (n + 1) * n / 2;
+            int pickSum = 0;
+            for (int num : ans) {
+                pickSum += num;
+            }
+            return Math.abs(pickSum - (sum - pickSum)) <= 1;
+        }
+    }
+
+    // 记忆化搜索
+    // 不是最优解，只是为了验证
+    // 返回能不能做到
+    public static boolean canSplit(int n, int k) {
+        int sum = (n + 1) * n / 2;
+        int wantSum = (sum / 2) + ((sum & 1) == 0 ? 0 : 1);
+        int[][][] dp = new int[n + 1][k + 1][wantSum + 1];
+        return f(n, 1, k, wantSum, dp);
+    }
+
+    public static boolean f(int n, int i, int k, int s, int[][][] dp) {
+        if (k < 0 || s < 0) {
+            return false;
+        }
+        if (i == n + 1) {
+            return k == 0 && s == 0;
+        }
+        if (dp[i][k][s] != 0) {
+            return dp[i][k][s] == 1;
+        }
+        boolean ans = f(n, i + 1, k, s, dp) || f(n, i + 1, k - 1, s - i, dp);
+        dp[i][k][s] = ans ? 1 : -1;
+        return ans;
+    }
+
 
 
     public static void main(String[] args) {
