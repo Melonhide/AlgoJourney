29503e9f2cbb61ea800ae98c9a40420a6753f20e
Melonhide <tt2830@columbia.edu>
2024-11-11 22:17:10 -0500

    Permutatin LCS Complete

--- src/class087/PermutationLCS.java
+++ src/class087/PermutationLCS.java
@@ -1,5 +1,7 @@
 package class087;
 
+import java.io.*;
+
 // 两个排列的最长公共子序列长度
 // 给出由1~n这些数字组成的两个排列
 // 求它们的最长公共子序列长度
@@ -9,5 +11,73 @@ package class087;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class PermutationLCS {
+    public static int n;
+    public static int[] a = new int[100001];
+    public static int[] b = new int[100001];
+    public static int[] where = new int[100001];
+    public static int[] ends = new int[100001];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken()!=StreamTokenizer.TT_EOF){
+            n = (int) in.nval;
+            in.nextToken();
+            for(int i = 0; i < n; i++){
+                a[i] = (int) in.nval;
+                in.nextToken();
+            }
+
+            for(int i = 0; i < n; i++){
+                b[i] = (int) in.nval;
+                in.nextToken();
+            }
+
+            out.println(compute());
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    public static int compute(){
+        for(int i = 0; i < n; i++){
+            where[a[i]] = i;
+        }
+
+        for(int i = 0; i < n; i++){
+            b[i] = where[b[i]];
+        }
+        int len = 0;
+        for(int i = 0, find; i < n; i++){
+            find = f(len, b[i]);
+            if(find == -1){
+                ends[len++] = b[i];
+            }else {
+                ends[find] = b[i];
+            }
+        }
+
+        return len;
+    }
+
+    public static int f(int len, int target){
+        int l = 0;
+        int r = len-1;
+        int ans = -1;
+        while(l<=r){
+            int m = (l+r)/2;
+            if(ends[m]>=target){
+                ans = m;
+                r = m-1;
+            }else{
+                l = m+1;
+            }
+
+        }
+
+        return ans;
+    }
+
 
 }
