a4f7c1d0cda6ca58113614edde5279896fc450a6
Melonhide <tt2830@columbia.edu>
2024-11-17 22:56:18 -0500

    090 question-complete

--- src/class090/CuttingBamboo.java
+++ src/class090/CuttingBamboo.java
@@ -7,4 +7,30 @@ package class090;
 // 答案需要对1000000007取模
 // 测试链接 : https://leetcode.cn/problems/jian-sheng-zi-ii-lcof/
 public class CuttingBamboo {
+    public static int cuttingBamboo(int n){
+        if(n == 2){
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+
+        int mod = 1000000007;
+        int tail = n%3 == 0? 1:(n%3 == 1? 4:2);
+        int p = tail == 1? n/3: (n-tail)/3;
+        return (int) ((power(3, p, mod)*tail)%mod);
+    }
+
+    public static long power(long x, int n, int mod){
+        long ans = 1;
+        while(n != 0){
+            if((n & 1) != 0){
+                ans = (ans * x) % mod;
+            }
+            x = (x*x)%mod;
+            n >>>= 1;
+        }
+        return ans;
+    }
+
 }
