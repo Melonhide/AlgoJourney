e2ee0c5fc9527ee9798d3825ae33024798cfe8d1
Melonhide <tt2830@columbia.edu>
2024-11-28 22:58:45 -0500

    Update 093 question

--- src/class093/CrossRiver.java
+++ src/class093/CrossRiver.java
@@ -1,6 +1,9 @@
 package class093;
 
 
+import java.io.*;
+import java.util.Arrays;
+
 // 过河问题
 // 一共n人出游，他们走到一条河的西岸，想要过河到东岸
 // 每个人都有一个渡河时间ti，西岸有一条船，一次最多乘坐两人
@@ -12,6 +15,47 @@ package class093;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class CrossRiver {
+    public static int maxn = 100001;
+    public static int n;
+    public static int[] nums = new int[maxn];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken()!=StreamTokenizer.TT_EOF){
+            n = (int) in.nval;
+            in.nextToken();
+            for(int i = 0; i < n; i++){
+                nums[i] = (int) in.nval;
+                in.nextToken();
+            }
+            out.println(compute());
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    public static int compute(){
+        Arrays.sort(nums, 0, n);
+        int[] dp = new int[n];
+        if(n>=1){
+            dp[0] = nums[0];
+        }
+        if(n>=2){
+            dp[1] = nums[1];
+        }
+        if(n>=3){
+            dp[2] = nums[2]+nums[0]+nums[1];
+        }
+
+        for(int i = 3; i < n; i++){
+            dp[i] = Math.min(nums[i]+nums[0]+dp[i-1], 2*nums[1]+nums[i]+nums[0]+dp[i-2]);
+        }
+
+        return dp[n-1];
+    }
+
 }
 
 
