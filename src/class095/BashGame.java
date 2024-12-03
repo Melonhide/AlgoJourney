c77730f3411d9a68020eae785c432fdb55979791
Melonhide <tt2830@columbia.edu>
2024-12-02 23:37:19 -0500

    Update bash game

--- src/class095/BashGame.java
+++ src/class095/BashGame.java
@@ -1,7 +1,18 @@
 package class095;
 
+import java.io.IOException;
+
 // 巴什博弈(Bash Game)
 // 一共有n颗石子，两个人轮流拿，每次可以拿1~m颗石子
 // 拿到最后一颗石子的人获胜，根据n、m返回谁赢
 public class BashGame {
+    public static String bashGame1(int n, int m){
+
+    }
+    public static String bashGame2(int n, int m){
+        return n%(m+1) == 0? "后手":"先手";
+    }
+    public static void main(String[] args) throws IOException{
+
+    }
 }
