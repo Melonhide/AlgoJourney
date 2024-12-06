7466c228ef2c830fda4348507cc6c81f88823713
Melonhide <tt2830@columbia.edu>
2024-12-05 22:03:37 -0500

    Nim game Complete

--- src/class095/NimGame.java
+++ src/class095/NimGame.java
@@ -1,6 +1,6 @@
 package class095;
 
-import java.io.IOException;
+import java.io.*;
 
 // 尼姆博弈(Nim Game)
 // 一共有n堆石头，两人轮流进行游戏
@@ -11,7 +11,28 @@ import java.io.IOException;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class NimGame {
-    public static void main(String[] args) throws IOException{
+    public static int t, n;
 
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken() != StreamTokenizer.TT_EOF){
+            t = (int) in.nval;
+            in.nextToken();
+            for(int i = 0; i < t; i++){
+                n = (int) in.nval;
+                in.nextToken();
+                int eor  = 0;
+                for(int j = 0; j < n; j++){
+                    eor ^= (int) in.nval;
+                    in.nextToken();
+                }
+                out.println(eor==0? "No":"Yes");
+            }
+        }
+        out.flush();
+        out.close();
+        br.close();
     }
 }
