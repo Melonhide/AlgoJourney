71d2b06dc039cacf972bff7f7e3f2731e1a94809
Melonhide <tt2830@columbia.edu>
2024-12-07 18:12:33 -0500

    WythoffGame Complete

--- src/class095/WythoffGame.java
+++ src/class095/WythoffGame.java
@@ -1,4 +1,8 @@
 package class095;
+
+import java.io.*;
+import java.math.BigDecimal;
+
 // 威佐夫博弈(Wythoff Game)
 // 有两堆石子，数量任意，可以不同，游戏开始由两个人轮流取石子
 // 游戏规定，每次有两种不同的取法
@@ -11,4 +15,30 @@ package class095;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class WythoffGame {
+    public static BigDecimal rate = new BigDecimal("1.61803398874989484");
+    public static int n,m;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken() != StreamTokenizer.TT_EOF){
+            n = (int) in.nval;
+            in.nextToken();
+            m = (int) in.nval;
+            out.println(compute());
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    public static int compute(){
+        int min = Math.min(m,n);
+        int max = Math.max(m,n);
+        if(min != rate.multiply(new BigDecimal(max-min)).intValue()){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
 }
