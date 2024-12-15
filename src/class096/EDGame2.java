20d36856eb021f9211a4e9aad67b6131a0e385f8
Melonhide <tt2830@columbia.edu>
2024-12-15 01:53:09 -0500

    ED Game Complete

--- src/class096/EDGame2.java
+++ src/class096/EDGame2.java
@@ -1,7 +1,7 @@
 package class096;
 
 
-import java.io.IOException;
+import java.io.*;
 
 // E&D游戏
 // 桌子上有2n堆石子，编号为1、2、3...2n
@@ -18,7 +18,41 @@ import java.io.IOException;
 // 这是输入输出处理效率很高的写法
 // 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class EDGame2 {
+    public static int t;
+    public static int n;
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while(in.nextToken() != StreamTokenizer.TT_EOF){
+            t = (int) in.nval;
+            in.nextToken();
+            for(int j = 0; j < t; j++){
+                n = (int) in.nval;
+                in.nextToken();
+                int ans = 0;
+                for(int i = 0, a, b; i < n; i+=2){
+                    a = (int) in.nval;
+                    in.nextToken();
+                    b = (int) in.nval;
+                    in.nextToken();
 
+                    ans ^= lowZero((a-1)|(b-1));
+                }
+                out.println(ans == 0? "NO":"YES");
+            }
+        }
+        out.flush();
+        out.close();
+        br.close();
+    }
+
+    public static int lowZero(int cur){
+        int ans = 0;
+        while((cur&1) != 0){
+            ans++;
+            cur >>>= 1;
+        }
+        return ans;
     }
 }
