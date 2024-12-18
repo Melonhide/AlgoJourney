4a61f03ff086531098c86f716d83797464b3a00f
Melonhide <tt2830@columbia.edu>
2024-12-17 22:22:17 -0500

    Input Long

--- src/class097/LargeNumberIsPrime1.java
+++ src/class097/LargeNumberIsPrime1.java
@@ -1,5 +1,7 @@
 package class097;
 
+import java.io.IOException;
+
 // 判断较大的数字是否是质数(Miller-Rabin测试)
 // 测试链接 : https://www.luogu.com.cn/problem/U148828
 // 如下代码无法通过所有测试用例
@@ -8,4 +10,8 @@ package class097;
 // 为什么不能搞定所有long类型的数字检查
 // 原因在于long类型位数不够，乘法同余的时候会溢出，课上已经做了说明
 public class LargeNumberIsPrime1 {
+
+    public static void main(String[] args) throws IOException{
+
+    }
 }
