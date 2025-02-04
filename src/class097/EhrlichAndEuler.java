79f08ce5c76a39645d105f72f0f906efbee88207
Melonhide <tt2830@columbia.edu>
2025-02-04 18:34:56 -0500

    count primes - ehrlich complete

--- src/class097/EhrlichAndEuler.java
+++ src/class097/EhrlichAndEuler.java
@@ -4,4 +4,27 @@ package class097;
 // 给定整数n，返回小于非负整数n的质数的数量
 // 测试链接 : https://leetcode.cn/problems/count-primes/
 public class EhrlichAndEuler {
+
+    public static int ehrlich(int n){
+        boolean[] visited = new boolean[n];
+        for(int i = 2; i < n; i++){
+            if(!visited[i]){
+                for(int j = i; j*i<n; j++){
+                    visited[i*j] = true;
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 2; i <n; i++){
+            ans += visited[i]? 0:1;
+        }
+
+        return ans;
+    }
+
+    public static int euler(int n) {
+
+    }
+
+
 }
