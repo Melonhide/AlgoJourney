7c299b5bc8b6d0f6ea64df32cb077fa35b3c47f3
Melonhide <tt2830@columbia.edu>
2025-02-16 22:32:54 -0500

    099 in progress

--- src/class099/InverseFactorial.java
+++ src/class099/InverseFactorial.java
@@ -8,4 +8,30 @@ public class InverseFactorial {
     public static int MOD = 1000000007;
     public static int LIMIT = 1000;
 
+    public static long[] fac = new long[LIMIT+1];
+
+    public static long[] inv = new long[LIMIT+1];
+
+    public static void build(){
+        fac[1] = 1;
+        for(int i = 2; i <= LIMIT; i++){
+            fac[i] = (fac[i-1] * i) % MOD;
+        }
+
+
+    }
+
+    public long power(long x, int n){
+        long ans = 1;
+        while(n>0){
+            if((n&1) == 1){
+                ans = (ans * x)%MOD;
+            }
+            x = (x*x)%MOD;
+            n>>=1;
+        }
+
+        return ans;
+    }
+
 }
