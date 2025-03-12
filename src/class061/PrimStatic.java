58c22a4a51673e04e7ef843c03be855ff6f2df9e
Melonhide <tt2830@columbia.edu>
2025-03-11 21:25:33 -0400

    Update 061 question

--- src/class061/PrimStatic.java
+++ src/class061/PrimStatic.java
@@ -1,6 +1,14 @@
 package class061;
 
 // Prim的反向索引堆实现，需要复习
+
+// Prim算法优化（洛谷）
+// 静态空间实现
+// 时间复杂度O(n + m) + O((m+n) * log n)
+// 测试链接 : https://www.luogu.com.cn/problem/P3366
+// 请同学们务必参考如下代码中关于输入、输出的处理
+// 这是输入输出处理效率很高的写法
+// 提交以下所有代码，把主类名改成Main，可以直接通过
 public class PrimStatic {
 
 
