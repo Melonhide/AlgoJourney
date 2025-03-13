39de66728e1f7ed55a79622e7657e412ab09ce4b
Melonhide <tt2830@columbia.edu>
2025-03-12 22:40:56 -0400

    Review Prim static

--- src/class061/PrimStatic.java
+++ src/class061/PrimStatic.java
@@ -2,6 +2,8 @@ package class061;
 
 // Prim的反向索引堆实现，需要复习
 
+import java.io.IOException;
+
 // Prim算法优化（洛谷）
 // 静态空间实现
 // 时间复杂度O(n + m) + O((m+n) * log n)
@@ -10,6 +12,8 @@ package class061;
 // 这是输入输出处理效率很高的写法
 // 提交以下所有代码，把主类名改成Main，可以直接通过
 public class PrimStatic {
+    public static void main(String[] args) throws IOException{
 
+    }
 
 }
