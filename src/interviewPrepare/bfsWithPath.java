fdff38d136d222348100d53ddaebcd4356b73739
Melonhide <tt2830@columbia.edu>
2025-04-26 12:13:43 -0400

    add real interview question section

---
+++ src/interviewPrepare/bfsWithPath.java
@@ -0,0 +1,10 @@
+package interviewPrepare;
+
+public class bfsWithPath {
+    public static int[] direction = new int[]{-1,0,1,0,-1}; // (x-1, y); (x, y+1); (x+1,y); (x,y-1);
+
+
+    public static void bfs_with_path(int[][] nums){
+
+    }
+}
