cf4684f41027072cecfe775de603813bb1f7557d
Melonhide <tt2830@columbia.edu>
2025-05-08 22:24:07 -0400

    String hash

--- src/class105/DifferentStrings.java
+++ src/class105/DifferentStrings.java
@@ -1,4 +1,9 @@
 package class105;
 
+// 统计有多少个不同的字符串
+// 测试链接 : https://www.luogu.com.cn/problem/P3370
+// 请同学们务必参考如下代码中关于输入、输出的处理
+// 这是输入输出处理效率很高的写法
+// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
 public class DifferentStrings {
 }
