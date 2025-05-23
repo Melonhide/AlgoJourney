e25309f1cecf8179631fc82a6c004a604fb17670
Melonhide <tt2830@columbia.edu>
2025-05-22 23:35:28 -0400

    update

--- src/interviewPrepare/mergeIntervalsVariant_56.java
+++ src/interviewPrepare/mergeIntervalsVariant_56.java
@@ -1,9 +1,11 @@
 package interviewPrepare;
 
+import java.util.List;
+
 public class mergeIntervalsVariant_56 {
     // Merge two sorted lists
     // Input [[1,3],[5,8],[11,15]]
     // Output
 
-    public
+    public List<int[]>
 }
