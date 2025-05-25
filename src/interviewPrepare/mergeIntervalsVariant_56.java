3c9beef29ba11fdc1648d494e1c20c3f01af4a99
Melonhide <tt2830@columbia.edu>
2025-05-24 22:52:18 -0400

    update

--- src/interviewPrepare/mergeIntervalsVariant_56.java
+++ src/interviewPrepare/mergeIntervalsVariant_56.java
@@ -1,5 +1,6 @@
 package interviewPrepare;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class mergeIntervalsVariant_56 {
@@ -7,7 +8,9 @@ public class mergeIntervalsVariant_56 {
     // Input [[1,3],[5,8],[11,15]]
     // Output
 
-    public List<int[]> mergeIntervals(){
+    public List<int[]> mergeIntervals(List<int[]> interval1, List<int[]> interval2){
+        List<int[] > ans = new ArrayList<>();
 
+        return ans;
     }
 }
