926bc0849a939419f9c6c374dc65025d70d6c552
Melonhide <tt2830@columbia.edu>
2025-05-29 23:24:21 -0400

    109

--- src/class109/DifferentColors.java
+++ src/class109/DifferentColors.java
@@ -1,4 +1,13 @@
 package class109;
-
+// HH的项链
+// 一共有n个位置，每个位置颜色给定，i位置的颜色是arr[i]
+// 一共有m个查询，question[i] = {li, ri}
+// 表示第i条查询想查arr[li..ri]范围上一共有多少种不同颜色
+// 返回每条查询的答案
+// 1 <= n、m、arr[i] <= 10^6
+// 1 <= li <= ri <= n
+// 测试链接 : https://www.luogu.com.cn/problem/P1972
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
+// 代码逻辑和课上讲的完全一致，但是重写了读写工具类，增加了io效率
 public class DifferentColors {
 }
