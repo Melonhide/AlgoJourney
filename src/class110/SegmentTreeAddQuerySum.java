694995b4ecb8ea08722cb7f750b57d80730bda20
Melonhide <tt2830@columbia.edu>
2025-05-30 23:57:43 -0400

    110

--- src/class110/SegmentTreeAddQuerySum.java
+++ src/class110/SegmentTreeAddQuerySum.java
@@ -1,4 +1,11 @@
 package class110;
 
+// 线段树支持范围增加、范围查询
+// 维护累加和
+// 测试链接 : https://www.luogu.com.cn/problem/P3372
+// 请同学们务必参考如下代码中关于输入、输出的处理
+// 这是输入输出处理效率很高的写法
+// 提交以下的code，提交时请把类名改成"Main"，可以直接通过
+
 public class SegmentTreeAddQuerySum {
 }
