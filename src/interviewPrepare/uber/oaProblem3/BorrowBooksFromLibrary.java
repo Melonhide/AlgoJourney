1b01b3dd1f929ac278b19906d4ddd5276dec0c3b
Melonhide <tt2830@columbia.edu>
2025-07-22 22:38:01 -0400

    Add uber oa

---
+++ src/interviewPrepare/uber/oaProblem3/BorrowBooksFromLibrary.java
@@ -0,0 +1,9 @@
+package interviewPrepare.uber.oaProblem3;
+
+// 图书馆借书，输入一个operations list，每个operation包含：行为，书名，数量，价格
+// 行为有三种：存入，借书，更新
+//1. 按照价格存入等量的书到图书馆。
+//2，借出一定量的书。同名书优先借出低价书。
+//3，更新等量书的价格
+public class BorrowBooksFromLibrary {
+}
---
+++ src/interviewPrepare/uber/oaProblem4/FindCommonPrefixFromTwoStringList.java
@@ -0,0 +1,8 @@
+package interviewPrepare.uber.oaProblem4;
+
+
+// 给两个string list，找两个string间最长的 common prefix。LC14变种。暴力解只能过一半的case。
+public class FindCommonPrefixFromTwoStringList {
+
+
+}
