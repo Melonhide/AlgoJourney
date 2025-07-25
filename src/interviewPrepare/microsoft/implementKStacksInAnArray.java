d0fee0085241f442070a8a45053f2623676a57f3
Melonhide <tt2830@columbia.edu>
2025-07-24 22:31:15 -0400

    implement k stacks in an array

--- src/interviewPrepare/microsoft/implementKStacksInAnArray.java
+++ src/interviewPrepare/microsoft/implementKStacksInAnArray.java
@@ -6,4 +6,42 @@ package interviewPrepare.microsoft;
 
 //Here i varies from 0 to k-1
 public class implementKStacksInAnArray {
+
+    static class stack2{
+        int[] arr;
+        int[] pointers;
+        int n;
+        int k;
+        public stack2(int n, int k){
+            this.n = n;
+            this.k = k;
+            arr = new int[n];
+            pointers = new int[k];
+            int stackSize = n/k;
+            for(int i = 0, p = 0; i < k; i++, p+=stackSize){
+                pointers[i] = p;
+            }
+        }
+
+        public void push(int x, int i){
+            arr[pointers[i]] = x;
+            pointers[i]++;
+        }
+
+        public int pop(int i) {
+            return arr[--pointers[i]];
+        }
+    }
+
+    public static void main(String[] args){
+        stack2 test = new stack2(12, 3);
+        test.push(19, 2);
+        test.push(1, 1);
+        test.push(193, 2);
+        test.push(9, 0);
+        test.push(29, 2);
+        System.out.println(test.pop(2));
+        System.out.println(test.pop(0));
+        System.out.println(test.pop(1));
+    }
 }
