629bd7c5e543e3c08d46346dedd6d2e984678173
Melonhide <tt2830@columbia.edu>
2025-07-29 22:40:41 -0400

    find the busiest processor

--- src/interviewPrepare/uber/oaProblem3/FindTheBusiestProcessor.java
+++ src/interviewPrepare/uber/oaProblem3/FindTheBusiestProcessor.java
@@ -21,5 +21,70 @@ public class FindTheBusiestProcessor {
     //
     //Your task is to return the index of the processor that processed the most instructions. If there is a tie, return the one with the smallest index.
 
+    public static int findBusiestProcessor(int[] capacity, String[] instructions){
+        int n = capacity.length;
+        boolean[] close = new boolean[n];
+        int[] cnt = new int[n];
+        int[] current = new int[n];
+        int total = 0;
+        int cur = 0;
+        for(int i = 0; i < n; i++){
+            total += capacity[i];
+        }
+        int available = total;
 
+        for(String i: instructions){
+            if(i.charAt(0) == 'p'){
+                if(available>0){
+                    while(cur<n && (close[cur] || current[cur] == capacity[cur])){
+                        cur++;
+                    }
+
+                }else{
+                    cur = 0;
+                    current = new int[n];
+                    available = total;
+                    while(cur < n && close[cur]) {
+                        cur++;
+                    }
+                }
+
+                cnt[cur]++;
+                current[cur]++;
+                available--;
+            }else{
+                StringBuilder ind = new StringBuilder();
+                int j = 0;
+                while(j<i.length() && i.charAt(j) != ' '){
+                    j++;
+                }
+                for(int k = j+1; k < i.length(); k++){
+                    ind.append(i.charAt(k));
+                }
+
+                int index = Integer.valueOf(ind.toString());
+                if(!close[index]){
+                    close[index] = true;
+                    total -= capacity[index];
+                    available -= capacity[index] - current[index];
+                }
+            }
+        }
+        int res = 0;
+        int curmax = 0;
+        for(int i = 0; i < n; i++){
+            if(cnt[i]>curmax){
+                curmax = cnt[i];
+                res = i;
+            }
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args){
+        int[] capacity = new int[]{1, 3, 1, 3, 4};
+        String[] instructions = new String[]{"package", "package", "closure 2", "package"};
+        System.out.println(findBusiestProcessor(capacity, instructions));
+    }
 }
